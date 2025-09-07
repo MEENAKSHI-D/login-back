95298081254bb94eaddc6979cc2ceedf1dcb9ad7
MEENAKSHI-D <meenakumara2025@gmail.com>
2025-09-07 18:04:43 +0530

    Remove node_modules

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const express = require ("express")
 const app = express();
 const cors = require("cors")
@@ -12,10 +10,10 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.post("/login",function(req,res)
+app.get("/login",function(req,res)
 {
-    console.log("BODY:", req.body);
-    if(req.body.username === username && req.body.password === password)
+    console.log("QUERY:", req.query);
+    if(req.query.username === username && req.query.password === password)
     {
         res.json(true)
 
