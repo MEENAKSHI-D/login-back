5286cfe36acaa9e1b372b451db2d833ae1414cba
MEENAKSHI-D <meenakumara2025@gmail.com>
2025-09-07 17:34:03 +0530

    Remove node_modules

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.post("/login",function(req,res)
         res.json(false)
     }
 })
-app.listen(5000, () => {
-  console.log(" Server running ....");
-});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
